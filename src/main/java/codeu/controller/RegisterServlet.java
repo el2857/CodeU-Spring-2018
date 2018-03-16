925d20db4bafdf2aa35be8627599544d1196bb51
Emily Li <el2857@columbia.edu>
2018-03-16 15:05:56 -0400

    added doPost() to RegisterServlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -1,4 +1,5 @@
 package codeu.controller;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +14,18 @@ public class RegisterServlet extends HttpServlet {
  @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
      throws IOException, ServletException {
-	
-	request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-	
+
+   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+ }
+
+ @Override
+ public void doPost(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletException {
+
+   String username = request.getParameter("username");
+   String password = request.getParameter("password");
+
+   response.getWriter().println("<p>Username: " + username + "</p>");
+   response.getWriter().println("<p>Password: " + password + "</p>");
  }
 }
