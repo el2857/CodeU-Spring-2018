caf9bbcabf558c9f8ded3bded9dad631aff6bd99
Emily Li <el2857@columbia.edu>
2018-03-17 17:42:23 -0400

    added UserStore class functionalities

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -5,11 +5,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import codeu.model.store.basic.UserStore;
+import codeu.model.data.User;
+import java.util.UUID;
+import java.time.Instant;
 
 /**
 * Servlet class responsible for user registration.
 */
 public class RegisterServlet extends HttpServlet {
+
+  /**
+  * Store class that gives access to Users.
+  */
+ private UserStore userStore;
+ 
+ /**
+  * Set up state for handling registration-related requests. This method is only called when
+  * running in a server, not when running in a test.
+  */
+ @Override
+ public void init() throws ServletException {
+   super.init();
+   setUserStore(UserStore.getInstance());
+ }
+ 
+ /**
+  * Sets the UserStore used by this servlet. This function provides a common setup method
+  * for use by the test framework or the servlet's init() function.
+  */
+ void setUserStore(UserStore userStore) {
+   this.userStore = userStore;
+ }
 
  @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -42,26 +69,6 @@ public class RegisterServlet extends HttpServlet {
 
    response.sendRedirect("/login");
  }
- 
-/**
-  * Store class that gives access to Users.
-  */
- private UserStore userStore;
- 
- /**
-  * Set up state for handling registration-related requests. This method is only called when
-  * running in a server, not when running in a test.
-  */
- @Override
- public void init() throws ServletException {
-   super.init();
-   setUserStore(UserStore.getInstance());
- }
- 
- /**
-  * Sets the UserStore used by this servlet. This function provides a common setup method
-  * for use by the test framework or the servlet's init() function.
-  */
- void setUserStore(UserStore userStore) {
-   this.userStore = userStore;
- }
+
+}
+
